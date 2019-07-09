a8b4e2e256bd05cd7f80022032a3f6be6e848ec0
Sergey Kosilov <sergey.cosilov@yandex.ru>
2019-07-09 13:16:25 +0300

    Initial Commit

---
+++ Shop of  Sofa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shop of  Sofa
+//
+//  Created by Сергей Косилов on 09/07/2019.
+//  Copyright © 2019 Сергей Косилов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
