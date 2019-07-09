d80747ddd275a1eef3c80460ec16ca962579f889
Sergey Kosilov <sergey.cosilov@yandex.ru>
2019-07-09 13:40:17 +0300

    Second

---
+++ Shop of  Sofa/model/Sofa.swift
@@ -0,0 +1,11 @@
+//
+//  Sofa.swift
+//  Shop of  Sofa
+//
+//  Created by Сергей Косилов on 09/07/2019.
+//  Copyright © 2019 Сергей Косилов. All rights reserved.
+//
+
+struct Sofa {
+    var name: String
+}
