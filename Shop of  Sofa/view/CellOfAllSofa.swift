16346122378ef01bafd8abb875e35890ba029ff4
Sergey Kosilov <sergey.cosilov@yandex.ru>
2019-07-09 17:04:49 +0300

    45

---
+++ Shop of  Sofa/view/CellOfAllSofa.swift
@@ -0,0 +1,21 @@
+//
+//  CellOfAllSofa.swift
+//  Shop of  Sofa
+//
+//  Created by Сергей Косилов on 09/07/2019.
+//  Copyright © 2019 Сергей Косилов. All rights reserved.
+//
+
+import UIKit
+
+class CellOfAllSofa: UITableViewCell {
+    
+    @IBOutlet weak var imageOfSofa: UIImageView!
+    
+    @IBOutlet weak var nameOfSofa: UILabel!
+    @IBOutlet weak var nameOfCollection: UILabel!
+    
+    @IBOutlet weak var prise: UILabel!
+    @IBOutlet weak var atributedButton: UIButton!
+    
+}
